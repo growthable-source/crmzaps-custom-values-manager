bf1d6f694d49adf01bb7d38f98d90d2097a16b1b
growthable-source <ryan@growthable.io>
2025-09-02 08:32:23 +1000

    Update server.js
    path missing

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
+const path = require('path'); 
 require('dotenv').config();
 
 const app = express();
